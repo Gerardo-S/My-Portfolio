3347a479089948e2d077a14025c02e846cfd0a19
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-05-22 23:05:45 -0700

    Fix icons directory bug with vercel

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,5 +1,5 @@
 export { default as IconGitHub } from "./github";
-export { default as IconLinkedin } from "./linkedin";
+export { default as IconLinkedin } from "./linkedIn";
 export { default as IconTwitter } from "./twitter";
 export { default as IconResume } from "./resume";
 export { default as IconExternal } from "./external";
